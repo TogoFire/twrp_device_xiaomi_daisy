318f0e4659cf61f0f6bc61c3310c0702d76a8f33
aragon12 <sm95007@gmail.com>
2018-10-04 20:28:41 +0530

    sakura: update for O-mr1

--- omni_sakura.mk
+++ omni_sakura.mk
@@ -1,17 +1,30 @@
-# Inherit from the common Open Source product configuration
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
-$(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
+#
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
-# Inherit some common CM stuff.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Release name
+PRODUCT_RELEASE_NAME := sakura
 
-# Inherit device configuration
-$(call inherit-product, device/xiaomi/sakura/device.mk)
+$(call inherit-product, build/target/product/embedded.mk)
 
-PRODUCT_NAME := omni_sakura
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sakura
+PRODUCT_NAME := omni_sakura
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 6 pro
+PRODUCT_MANUFACTURER := Xiaomi

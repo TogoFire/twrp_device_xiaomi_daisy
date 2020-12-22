23ebd452405186b88906a30e93c81e5a9dcc98c6
TogoFire <italomellopereira@gmail.com>
2020-12-22 17:38:37 -0300

    daisy: Orangefox-ify!
    Changelog:
    Add Rounded Corners;
    export OF_STATUS_INDENT_LEFT=48
    export OF_STATUS_INDENT_RIGHT=48
    
    Disable the Green LED switching on settings;
    * Daisy use white colour for all notification.
    export OF_USE_GREEN_LED=0
    
    Disable saving of previously recovery logs;
    * Some users don't like it; Users use the last recovery log more often
    export OF_DONT_KEEP_LOG_HISTORY=1
    
    Fix screen size parameter;
    * Mi A2 Lite - Resolution: 1080 x 2280 pixels, 19:9 ratio (~432 ppi density). OF_SCREEN_H := 2280
    
    "OF_SCREEN_H"
      - Use this if your screen's aspect ratio is not 16:9
      - to calculate OF_SCREEN_H, if your screen width is not 1080, use this formula: <aspect ratio height>*120
      - e.g. if the aspect ratio is 19:9 then use 19*120 (=2280)
      - use "export OF_SCREEN_H=2280" to resize recovery to 19:9 screen
      - default = 1920
    
    statusbar/cutout, clock;
    * OF_STATUS_H=95  (cutout)
    * OF_CLOCK_POS=1  (1 => left and right clock positions available)
    
    Source: https://gitlab.com/OrangeFox/vendor/recovery/-/blob/master/orangefox_build_vars.txt
    
    Update kernel to Panda-4.9.248 - Source: https://github.com/TogoFire/kernel_xiaomi_panda
    Update dependencies;
    Add vendorsetup.sh;
    Add extra checks to ensure that this script is executed properly;
    Drop specific theme portrait_hdpi_daisy;
    Update text in installer update-binary  ui_print;
    
    Workaround for flashing ROMs;
    export FOX_BUGGED_AOSP_ARB_WORKAROUND="1451606400"
    
    *Without this fix, only roms built after building OF can be flashed on the phone. With this workaround builds made after 1451606400 (1 January 2016) will be able to flash.
    
    -log: E update_engine_sideload: [0711/062952.541871:ERROR:delta_performer.cc(1452)] The current OS build timestamp (1608411114) is newer than the maximum timestamp in the manifest (1605445951)
    
    Change-Id: be1b4fda13a37da73492723797c7a16d30ed80b0
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,25 +1,122 @@
 #
-# Copyright 2017 The Android Open Source Project
+#	This file is part of the OrangeFox Recovery Project
+# 	Copyright (C) 2020 The OrangeFox Recovery Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 	This software is released under GPL version 3 or any later version.
+#	See <http://www.gnu.org/licenses/>.
 #
+# 	Please maintain this if you use this script or any part of it
+#
+#	!/bin/bash
+#	put script in xtended source folder, make executable (chmod +x vendorsetup.sh) and run it (./vendorsetup.sh)
+#
+FDEVICE="daisy"
 
-# This file is executed by build/envsetup.sh, and can use anything
-# defined in envsetup.sh.
-#
-# In particular, you can add lunch options with the add_lunch_combo
-# function: add_lunch_combo generic-eng
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
 
-for var in eng user userdebug; do
-  add_lunch_combo omni_daisy-$var
-done
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export OF_AB_DEVICE=1
+	export OF_DISABLE_MIUI_SPECIFIC_FEATURES=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+	export OF_USE_MAGISKBOOT="1"
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export OF_USE_GREEN_LED=0
+	export OF_DONT_KEEP_LOG_HISTORY=1
+
+  #export LC_ALL=1
+	#export ALLOW_MISSING_DEPENDENCIES=1
+	#export FOX_USE_TAR_BINARY=1
+	#export FOX_USE_ZIP_BINARY=1
+	#export FOX_REPLACE_BUSYBOX_PS=1
+	#export OF_DISABLE_DM_VERITY_FORCED_ENCRYPTION="1"; # disabling dm-verity causes stability issues with some kernel 4.9 ROMs; but is needed for MIUI
+	#export OF_DISABLE_FORCED_ENCRYPTION=1
+	#export FOX_RECOVERY_INSTALL_PARTITION=/dev/block/platform/msm_sdcc.1/by-name/recovery
+	#export OF_VANILLA_BUILD=1
+	#export OF_SKIP_ORANGEFOX_PROCESS=1
+	#export OF_DONT_PATCH_ON_FRESH_INSTALLATION=1
+	#export OF_FORCE_MAGISKBOOT_BOOT_PATCH_MIUI="1"
+	#export OF_NO_MIUI_OTA_VENDOR_BACKUP="1"
+	#export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
+	#export OF_FBE_METADATA_MOUNT_IGNORE=1
+	#export FOX_BUILD_FULL_KERNEL_SOURCES=1
+	#export FOX_USE_LZMA_COMPRESSION=1
+	#export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	#export PLATFORM_VERSION="16.1.0"
+	#export PLATFORM_SECURITY_PATCH="2099-12-31"
+	#export TW_DEFAULT_LANGUAGE="en"
+
+	# MAINTAINER
+	#export FOX_BUILD_TYPE=Stable
+	export OF_MAINTAINER=TogoFire
+	#export FOX_VERSION=R11.0
+
+	# OTA for custom ROMs
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+
+	# use system (ROM) fingerprint where available
+	#export OF_USE_SYSTEM_FINGERPRINT=1
+
+	# Rounded corners
+	export OF_STATUS_INDENT_LEFT=48
+	export OF_STATUS_INDENT_RIGHT=48
+
+	# Screen
+	export OF_SCREEN_H=2280
+
+	# CLOCK
+	export OF_CLOCK_POS=1
+
+	# Status
+	export OF_STATUS_H=95
+
+  # Workaround for flashing ROMs
+  export FOX_BUGGED_AOSP_ARB_WORKAROUND="1451606400"
+
+	# -- add settings for R11 --
+	export FOX_R11=1
+	export OF_USE_TWRP_SAR_DETECT=1
+	export FOX_ADVANCED_SECURITY=1
+	#export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+	#export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+	# -- end R11 settings --
+
+	# let's log what are the build VARs that we used
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "PLATFORM_" >> $FOX_BUILD_LOG_FILE
+  	fi
+
+	add_lunch_combo omni_"$FDEVICE"-eng
+	#add_lunch_combo omni_"$FDEVICE"-userdebug
+fi
+#

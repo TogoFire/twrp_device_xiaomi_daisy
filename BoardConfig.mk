b209d88b527a10637a29f2e1d44ddba53d1ae5ee
Darth9 <reyprimerjefe@gmail.com>
2021-04-21 17:33:24 +0100

    Remove useless flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -102,7 +102,6 @@ TW_MAX_BRIGHTNESS := 255
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXTRA_LANGUAGES := true
-TW_INCLUDE_FUSE_NTFS := true
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_SCREEN_BLANK_ON_BOOT := true

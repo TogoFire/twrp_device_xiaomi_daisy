34b1f211c65c60af57ea9142dbf3eca1b70718a7
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-21 21:46:16 +0800

    tissot: Merge fstab from common device tree

--- device.mk
+++ device.mk
@@ -8,6 +8,9 @@
 MITHORIUM_INCLUDE_CRYPTO_FDE := true
 MITHORIUM_LEGACY_CRYPTO := true
 
+# Fstab
+MITHORIUM_USES_DEVICE_SPECIFIC_FSTAB := true
+
 # Inherit from mithorium-common
 $(call inherit-product, device/xiaomi/mithorium-common/mithorium.mk)
 

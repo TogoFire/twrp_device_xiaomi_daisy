296a091639db0d255b8638feeee13d8f944ffa72
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-01 02:08:35 +0800

    tissot: Switch to ICE Encryption mode for FBE
    This device supports it surprisingly

--- recovery/root/system/bin/detect-fbe.sh
+++ recovery/root/system/bin/detect-fbe.sh
@@ -10,8 +10,8 @@ if mount -o ro /dev/block/bootdevaes-256-xts/by-name/userdata /data; then
         sed -i 's|encryptable=footer;||g' /system/etc/twrp.flags
     else
         # FBE
-        sed -i 's|,encryptable=footer|,fileencryption=aes-256-xts|g' /system/etc/recovery.fstab
-        sed -i 's|encryptable=footer;|fileencryption=aes-256-xts;|g' /system/etc/twrp.flags
+        sed -i 's|,encryptable=footer|,fileencryption=ice|g' /system/etc/recovery.fstab
+        sed -i 's|encryptable=footer;|fileencryption=ice;|g' /system/etc/twrp.flags
     fi
     umount /data || true
 fi
